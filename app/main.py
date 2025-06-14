7890a87abc69c3884bfeff65bfbceea2ac7c7477
afshin-8 <a.mokhlesi@gmail.com>
2025-06-14 15:05:14 +0000

    Test full CI/CD flow

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return" CI is working! Code updated successfully."
+    return" CI/CD is working! Code updated successfully."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
