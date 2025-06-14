154ad3bbe1f3fb8308900b9c6f0418c1ffc3c431
afshin-8 <a.mokhlesi@gmail.com>
2025-06-14 16:45:02 +0000

    Test CD with active runner again

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return"CD with self-hosted runner is working correct!"
+    return"CD with self-hosted runner is working correct full!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
