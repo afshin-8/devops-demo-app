beff5f89b7ff59ca5720822899c4d5184e47a5ed
afshin-8 <a.mokhlesi@gmail.com>
2025-06-14 15:53:52 +0000

    Test CD with self-hosted runner after DNS fix

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return" CI/CD is working! Code updated successfully."
+    return"CD with self-hosted runner is working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
